6d7f00e87fdd9000849657975e184125c35899c5
MrMallik <mallik.pritabrata@gmail.com>
2022-07-18 17:01:45 +0530

    fixed formatting

--- evaluator.py
+++ evaluator.py
@@ -1,10 +1,10 @@
-'''
+"""
 Here's the application flow - 
 
 1. The accepted user string is sent to the tokenizer
 2. A list containing the tokens are returned
 3. Finally, this file evaluator.py processes the list and evaluates the expression
-'''
+"""
 
 
 from random import choice
@@ -13,26 +13,23 @@ from sys import argv
 
 
 def concatenate(words):
-    
+
     ans = ""
 
     for word in words:
-        ans += word
+        ans = word
 
     return ans
 
 
 def join(words):
 
-    
     return words[-1].join(words[:-1])
 
 
 def pickRandom(words):
-    
 
     return choice(words)
-
 
 
 def evaluateExpression(s):
@@ -42,58 +39,56 @@ def evaluateExpression(s):
 
     for functions in myTokenizedStck:
 
-        if functions.key == 'join':
+        if functions.key == "join":
             if len(functions.val) == 0:
-                ans+=join(stck)
+                ans = join(stck)
                 stck = []
             else:
-                ans += join(functions.val)
+                ans = join(functions.val)
                 stck.append(ans)
 
-        elif functions.key == 'concatenate':
+        elif functions.key == "concatenate":
             if len(functions.val) == 0:
-                ans+=concatenate(stck)
+                ans = concatenate(stck)
                 stck = []
             else:
-                ans += concatenate(functions.val)
+                ans = concatenate(functions.val)
                 stck.append(ans)
 
-        elif functions.key == 'pickRandom':
+        elif functions.key == "pickRandom":
             if len(functions.val) == 0:
-                ans+=pickRandom(stck)
+                ans = pickRandom(stck)
                 stck = []
             else:
-                ans += pickRandom(functions.val)
+                ans = pickRandom(functions.val)
                 stck.append(concatenate(functions.val))
-         
 
     return ans
 
 
 def isValidStrLiteral(str):
-    return str.count("\'") % 2 == 0 and (str[0] == '\'' and str[-1] == '\'')
+    return str.count("'") % 2 == 0 and (str[0] == "'" and str[-1] == "'")
 
 
 # expr = "concatenate(pickRandom('Fruits: '), join('apple', 'grape', 'banana', 'orange', ', '))"
 # expr = "pickRandom(concatenate(pickRandom('stupid')), concatenate('fax'))" # -- FAILS
 # expr = "join(concatenate('hola'), join('apple', 'grape', 'banana', 'orange', ', '), '^_^')" # -- FAILS
-# expr = "concatenate('bhalo', concatenate('theko'))" 
+# expr = "concatenate('bhalo', concatenate('theko'))"
 # expr = "concatenate(concatenate('bhalo'), ' ', 'theko')" # -- FAILS
 # expr = "concatenate('Hello', ' ', pickRandom('Pritabrata', 'Ravi', 'Ramana'))"
 # expr = "concatenate(foo(), 'Fruits: ', join('apple', 'grape', 'banana', 'orange', ', '))"
 
 expr = str(argv[1])
 
-if expr.startswith("\'"):
-        if isValidStrLiteral(expr):
-            print(expr)
-        else:
-            print("You've entered an invalid string literal. a string literal is any set of characters except single quote between a pair of single quotes")
+if expr.startswith("'"):
+    if isValidStrLiteral(expr):
+        print(expr)
+    else:
+        print(
+            "You've entered an invalid string literal. a string literal is any set of characters except single quote between a pair of single quotes"
+        )
 
 try:
     print(evaluateExpression(expr))
 except Exception as e:
     print(e)
-
-
-
--- tokenizer.py
+++ tokenizer.py
@@ -1,24 +1,26 @@
 import ast
 
-class StackElement:
 
+class StackElement:
     def __init__(self, functionName, params):
         self.key = functionName
         self.val = params
 
+
 # right is the top of the stack
 stack = []
+
 
 class ExpressionVisitor(ast.NodeVisitor):
     def visit_Call(self, node):
 
-        if node.func.id not in ['join', 'concatenate', 'pickRandom']:
+        if node.func.id not in ["join", "concatenate", "pickRandom"]:
             raise Exception("Unsupported Method Invocation")
-        elif node.func.id == 'join' and len(node.args) < 3:
+        elif node.func.id == "join" and len(node.args) < 3:
             raise Exception("Minimum of 3 parameters necessary for join()")
-        elif node.func.id == 'concatenate' and len(node.args) < 1:
+        elif node.func.id == "concatenate" and len(node.args) < 1:
             raise Exception("Minimum 1 parameter necessary for concatenate()")
-        elif node.func.id == 'pickRandom' and len(node.args) < 1:
+        elif node.func.id == "pickRandom" and len(node.args) < 1:
             raise Exception("Minimum of 1 parameter necessary for pickRandom()")
 
         args = []
@@ -26,7 +28,7 @@ class ExpressionVisitor(ast.NodeVisitor):
             if isinstance(i, ast.Constant):
                 args.append(i.value)
 
-        #arg-list will only have constants, not function calls
+        # arg-list will only have constants, not function calls
 
         stack.append(StackElement(node.func.id, args))
         self.generic_visit(node)
@@ -35,23 +37,5 @@ class ExpressionVisitor(ast.NodeVisitor):
 def getTokenizedStack(expr):
     # print(ast.dump(ast.parse(expr)))
     ExpressionVisitor().visit(ast.parse(expr))
-    
+
     return stack
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
